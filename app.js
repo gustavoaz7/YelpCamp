d8205e7a07c879b5d1304ab2b8435f31ef5fd020
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-11-09 20:39:50 -0200

    Updated
    Added new route to post new campgrounds

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
 
 app.set('view engine', 'ejs')
+app.use(bodyparser.urlencoded({extended: true}))
 
 app.get('/', (req,res) => {
   res.render("landing")
 })
 
-app.get('/campgrounds', (req, res) => {
-  let campgrounds = [
-    {name: 'Garatucaia', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Deep_Lake_tenting_campsite_-_Riding_Mountain_National_Park.JPG'},
-    {name: 'Three Cliffs', image: 'http://farm2.static.flickr.com/1007/575816446_eb75bcab37.jpg'},
-    {name: 'Visconde de Maua', image: 'https://www.outsideonline.com/sites/default/files/styles/full-page/public/2017/07/18/picking-campsite_h.jpg'}
-  ]
+// TEMP
+let campgrounds = [
+  {name: 'Garatucaia', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Deep_Lake_tenting_campsite_-_Riding_Mountain_National_Park.JPG'},
+  {name: 'Three Cliffs', image: 'http://farm2.static.flickr.com/1007/575816446_eb75bcab37.jpg'},
+  {name: 'Visconde de Maua', image: 'https://www.outsideonline.com/sites/default/files/styles/full-page/public/2017/07/18/picking-campsite_h.jpg'},
+  {name: 'Garatucaia', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Deep_Lake_tenting_campsite_-_Riding_Mountain_National_Park.JPG'},
+  {name: 'Three Cliffs', image: 'http://farm2.static.flickr.com/1007/575816446_eb75bcab37.jpg'},
+  {name: 'Garatucaia', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Deep_Lake_tenting_campsite_-_Riding_Mountain_National_Park.JPG'},
+  {name: 'Three Cliffs', image: 'http://farm2.static.flickr.com/1007/575816446_eb75bcab37.jpg'}
+]
 
+
+app.get('/campgrounds', (req, res) => {
+  
   res.render("campgrounds", {campgrounds: campgrounds})
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new.ejs')
+})
+
+app.post('/campgrounds', (req, res) => {
+  const newCampground = {
+    name: req.body.name,
+    image: req.body.image
+  } 
+  campgrounds.push(newCampground)
+
+  res.redirect('/campgrounds')
 })
 
 app.listen(3000, function() {
