38e2c84ab518e8a443066a27621a917e7341b92a
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-11-16 23:27:38 -0200

    Error handling & deleting comment from campground collection as well

--- routes/comments.js
+++ routes/comments.js
@@ -7,7 +7,7 @@ const flash = require('connect-flash')
 // New comment form
 router.get('/new', isLoggedIn, (req, res) => {
   Campground.findById(req.params.id, (err, campground) => {
-    if (err) throw err;
+    if (err) return console.log(err);
     res.render('newComment', {campground: campground})
   })
 })
@@ -15,10 +15,13 @@ router.get('/new', isLoggedIn, (req, res) => {
 // New comment form logic
 router.post('/', isLoggedIn, (req, res) => {
   Campground.findById(req.params.id, (err, campground) => {
-    if (err) res.redirect('/campgrounds')
+    if (err || !campground) {
+      console.log(err);
+      return res.redirect('/campgrounds')
+    }
     Comment.create(req.body.comment, (err, comment) => {  
       // req.body.commment is available because of comment[text] & comment[author] on newComment.ejs
-      if (err) {
+      if (err || !comment) {
         req.flash('error', err)
       } else {
         comment.author = {id: req.user._id, username: req.user.username }
@@ -35,7 +38,10 @@ router.post('/', isLoggedIn, (req, res) => {
 // Edit comment form
 router.get('/:comment_id/edit', isAuthorOfComment, (req, res) => {
   Comment.findById(req.params.comment_id, (err, comment) => {
-    if (err) res.redirect('back');
+    if (err || !comment) {
+      console.log(err);
+      return res.redirect('back');
+    }
     res.render('editComment', {comment: comment, campgroundID: req.params.id})
   })
 })
@@ -43,17 +49,38 @@ router.get('/:comment_id/edit', isAuthorOfComment, (req, res) => {
 // Update comment logic
 router.put('/:comment_id', isAuthorOfComment, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.updatedComment, (err, comment) => {
-    req.flash('success', 'Comment successfully updated.')
+    if (err || !comment) {
+      console.log(err);
+      req.flash('error', 'Failed to update comment.')
+    } else {
+      req.flash('success', 'Comment successfully updated.')
+    }
     res.redirect(`/campgrounds/${req.params.id}`)
   })
 })
 
 // Remove comment
 router.delete('/:comment_id', isAuthorOfComment, (req, res) => {
-  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
-    req.flash('success', 'Comment successfully deleted.')
-    res.redirect(`/campgrounds/${req.params.id}`)
+  Comment.findByIdAndRemove(req.params.comment_id, (err, deletedComment) => {
+    if (err) {
+      console.log(err);
+      req.flash('error', err.message)
+      return res.redirect(`/campgrounds/${req.params.id}`)
+    }
+    Campground.findByIdAndUpdate(req.params.id, {
+      $pull: {
+        comments: deletedComment.id  // Pulls deletedComment from campground's comments array
+      }
+    }, (err, updatedCampground) => {
+      if (err) {
+        console.log(err);
+        req.flash('error', err.message)
+        return res.redirect(`/campgrounds/${req.params.id}`)
+      }
+      req.flash('success', 'Comment successfully deleted.')
+    })
   })
+  res.redirect(`/campgrounds/${req.params.id}`)
 })
 
 // middleware
@@ -66,16 +93,18 @@ function isLoggedIn(req, res, next) {
 function isAuthorOfComment(req, res, next) {
   if (req.isAuthenticated()) {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
-      if (err) {
+      if (err || !foundComment) {
+        console.log(err);
         req.flash('error', "Comment not found.")
         res.redirect('back')
       } else if (foundComment.author.id.equals(req.user._id)) return next();
+      // .equals is a mongoose method. === wouldn't work because one is an object and the other a string.
       req.flash('error', "You don't have permission to do that.")
       res.redirect('back')
     })
   } else {
     req.flash('error', "You must log in first.")
-    res.redirect('back')
+    res.redirect('/login')
   }
 }
 
