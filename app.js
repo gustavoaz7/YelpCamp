af2376dc19fe22331d4b431e39a6a446b483e7bd
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-11-20 00:20:16 -0200

    Added passport-facebook & refactored configuration

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Requiring packages
 const express = require('express')
-const app = express()
 const bodyparser = require('body-parser')
 const session = require('express-session')
 const flash = require('connect-flash')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
@@ -13,17 +14,33 @@ const methodOverride = require('method-override')
 const User = require('./models/user.js')
 const Campground = require('./models/campground.js')
 const Comment = require('./models/comment.js')
-//const seedDB = require('./seeds.js')  
-//seedDB();   // Clear DB and populate with 3 items
 // Requiring routes
 const campgroundRoutes = require('./routes/campgrounds.js')
 const commentRoutes = require('./routes/comments.js')
 const authRoutes = require('./routes/auth.js')
 
-// New mongoose connection logic
-mongoose.connect('mongodb://localhost/yelpcamp_db', { useMongoClient: true })
-// tell Mongoose to use Node global es6 Promises
+// DATABASE CONFIG
+const configDB = require('./config/database.js')
+  // tell Mongoose to use Node global es6 Promises
 mongoose.Promise = global.Promise;
+  // New mongoose connection logic
+mongoose.connect(configDB.url, { useMongoClient: true })
+  .then(() => console.log('Database connected successfully.'))
+  .catch(err => console.log('Error connecting to database: ' + err.message))
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// CONFIG EXPRESS APP
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+app.use(bodyparser.json())  // get information from html forms
+app.use(bodyparser.urlencoded({extended: true}))  
+app.use(flash())  
+app.use(methodOverride('_method'))
+app.use(morgan('dev'));  // log every request to the console
+app.use(cookieParser());  // read cookies (needed for auth)
+
 
 // PASSPORT CONFIG
 app.use(session({
@@ -32,23 +49,19 @@ app.use(session({
   saveUninitialized: false
 }))
 app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+app.use(passport.session())  // persistent login sessions - MUST be after the session from express-session to use it.
 
-// CONFIG
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
-app.use(bodyparser.urlencoded({extended: true}))
-app.use(flash())
+
 app.use(function(req, res, next) {
-  res.locals.loggedUser = req.user;   // req.locals will be available in all our templaces
+  // req.locals holds variables that are available only to the views rendered during that request.
+  res.locals.loggedUser = req.user;
   res.locals.flashError = req.flash('error');
   res.locals.flashSuccess = req.flash('success');
   next()
 })
-app.use(methodOverride('_method'))
+// app.locals holds variables within and throughout the life of the app.
+app.locals.moment = require('moment')  
+
 
 // ROUTES
 app.use("/", authRoutes)
